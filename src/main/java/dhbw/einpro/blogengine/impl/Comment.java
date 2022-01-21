822108e4aaddb5984dd0904cf4b8db8f0fe61460
Nina <wi19171@lehre.dhbw-stuttgart.de>
2022-01-21 15:41:57 +0100

    Comment class implemented

--- src/main/java/dhbw/einpro/blogengine/impl/Comment.java
+++ src/main/java/dhbw/einpro/blogengine/impl/Comment.java
@@ -12,6 +12,39 @@ import dhbw.einpro.blogengine.interfaces.IUser;
  */
 public class Comment implements IComment
 {
+    private String content;
+    private IUser author;
+    private IPost post;
+
+    @Override
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public void setContent(String p_content) throws IllegalOperationException {
+        if (p_content.length() < 256) {
+            this.content = p_content;
+        }
+        else {
+            throw new IllegalOperationException("Content has more than 256 characters");
+        }
+    }
+
+    @Override
+    public IUser getAuthor() {
+        return author;
+    }
+
+    @Override
+    public void setAuthor(IUser p_author) {
+        this.author = p_author;
+    }
+
+    @Override
+    public void setPost(IPost p_post) {
+        this.post = p_post;
+    }
 
 
 }
